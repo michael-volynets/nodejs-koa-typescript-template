72c6834703a1b95b610a4b8b3b6aef5874ec198d
MishaVolinets <volinetsmisha@gmail.com>
2019-05-12 02:01:32 +0300

    Created Todo Endpoints

--- src/routes/api/todo.ts
+++ src/routes/api/todo.ts
@@ -13,8 +13,68 @@ const createTodo = async (ctx: RouterContext) => {
     ctx.body = result;
 };
 
+const getTodos = async (ctx: RouterContext) => {
+    let skip = 0;
+    let limit = 20;
+
+    if (ctx.request.query.skip) {
+        const parsed = Number.parseInt(ctx.request.query.skip);
+
+        if (typeof parsed === 'number') {
+            skip = parsed;
+        }
+    }
+
+    if (ctx.request.query.limit) {
+        const parsed = Number.parseInt(ctx.request.query.limit);
+
+        if (typeof parsed === 'number') {
+            limit = parsed;
+        }
+    }
+    
+    ctx.body = await Todo
+        .find()
+        .skip(skip)
+        .limit(limit);
+};
+
+const getTodo = async (ctx: RouterContext) => {
+    const todoId = ctx.params.todoId;
+
+   ctx.body = await Todo.findById(todoId);
+
+   if (!ctx.body) {
+       ctx.status = 404;
+   }
+};
+
+const updateTodo = async (ctx: RouterContext) => {
+    const todoId = ctx.params.todoId;
+
+    await Todo.updateOne({
+        _id: todoId
+    }, ctx.request.body);
+
+    ctx.status = 200;
+};
+
+const deleteTodo =  async (ctx: RouterContext) => {
+    const todoId = ctx.params.todoId;
+
+    await Todo.deleteOne({
+        _id: todoId
+    });
+
+    ctx.status = 200;
+};
+
 // Routes
 
-router.post("/",createTodo)
+router.get("/", getTodos);
+router.get("/:todoId", getTodo);
+router.post("/", createTodo);
+router.put("/:todoId", updateTodo);
+router.delete("/:todoId", deleteTodo);
 
 export default router;
